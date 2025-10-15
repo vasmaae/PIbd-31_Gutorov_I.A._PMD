6d99ce3be320ecd07f99e4d53b25a8d14837629b
Ivan Gutorov <gutorov.ia1@gmail.com>
2025-10-15 19:34:18 +0400

    add lab 3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pibd_31_gutorov_i_a_pmd/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ToDoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
